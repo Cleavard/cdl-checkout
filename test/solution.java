41c6aa6f4969dd9183c5fa6053e26b587eebd798
Richard Cleave <rcleave832@gmail.com>
2023-10-11 11:06:54 +0000

    Created initial testing framework

--- test/solution.java
+++ test/solution.java
@@ -1,10 +1,37 @@
 package test;
-import org.junit.*;;
+
+import org.junit.*;
+import org.junit.runners.AllTests;
 
 
 public class solution {
 
+    // Test cases for core functionality
+    
     @Test
-    public void test(){}
+    public void testStartup(){}
 
+    @Test
+    public void testStartupInvalid(){}
+
+    @Test
+    public void testStartupEmpty(){}
+
+    @Test
+    public void testStartupFinish(){}
+
+    @Test
+    public void testItemScan(){}
+
+    @Test
+    public void testItemScanInvalid(){}
+
+    @Test
+    public void testItemScanDeal(){}
+
+    @Test
+    public void testFinalTotal(){}
+
+    @Test 
+    public void testFinalTotalEmpty(){}
 }
