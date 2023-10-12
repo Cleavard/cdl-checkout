a483362eb47b8d993b47e22a9410d2058bfcd4c8
Richard Cleave <rcleave832@gmail.com>
2023-10-12 15:44:32 +0000

    New files from mvm refactor

---
+++ src/main/java/com/cdl/checkout/rcsolution/ItemPrice.java
@@ -0,0 +1,20 @@
+package com.cdl.checkout.rcsolution;
+
+public class ItemPrice {
+    private double price;
+    private int specialAmount = 0;
+    private double specialPrice;
+
+    public ItemPrice(double price, int specialAmount, double specialPrice) {
+        this.price = price;
+        this.specialAmount = specialAmount;
+        this.specialPrice = specialPrice;
+    }
+
+    public double getPrice() { return price; }
+
+    public int getSpecialAmount() { return  specialAmount; }
+
+    public double getSpecialPrice() { return specialPrice; }
+}
+
