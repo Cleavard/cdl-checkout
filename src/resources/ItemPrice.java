91e886f0c7c8567cd06e5800fbdc6acbfb91cd0e
Richard Cleave <rcleave832@gmail.com>
2023-10-12 10:38:51 +0000

    Refactored directories

---
+++ src/resources/ItemPrice.java
@@ -0,0 +1,21 @@
+package src.resources;
+
+
+public class ItemPrice {
+    private double price;
+    private int specialAmount = 0;
+    private double specialPrice;
+
+    public ItemPrice(double price, int specialAmount, double specialPrice) {
+        this.price = price;
+        this.specialAmount = specialAmount;
+        this.specialPrice = specialPrice;
+    }
+
+    public double getPrice() { return price; }
+
+    public int getSpecialAmount() { return  specialAmount; }
+
+    public double getSpecialPrice() { return specialPrice; }
+}
+
